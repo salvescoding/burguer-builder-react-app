39ad2cad4b7df3e0293b4a5c6967cddd09b773da
salvescoding <salves2383@gmail.com>
2018-11-29 18:31:17 +0100

    Implement CSS module for Layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import Aux from './hoc/Aux';
+import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 
 class App extends Component {
   render() {
     return (
       <Aux>
         <Layout>
-          <p>Test</p>
+          <BurguerBuilder />
         </Layout>
       </Aux>
     );
---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurguerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burguer</div>  
+                <div>Build controls</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurguerBuilder;
