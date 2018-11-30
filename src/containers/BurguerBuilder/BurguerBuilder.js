5260449ca491288d3e826a0118ec12a1490f5e91
salvescoding <salves2383@gmail.com>
2018-11-30 17:59:42 +0100

    Add Prop types package, Implement BurgerIngredient component and Burger component

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurguerBuilder extends Component {
     render() {
         return (
             <Aux>
-                <div>Burguer</div>  
-                <div>Build controls</div>
+                <Burger />
             </Aux>
         )
     }
